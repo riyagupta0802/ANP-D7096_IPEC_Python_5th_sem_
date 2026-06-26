f944d5542775c33d7bfa07c126654d9173db6c38
riyagupta0802 <riyaguptaa2006@gmail.com>
2026-06-26 12:07:09 +0530

    Added greatest_number

---
+++ classwork/day-3_Selection_statement_26th_June/greatest_number.py
@@ -0,0 +1,24 @@
+'''Program to find out the greatest number between two numbers'''
+#input of first number from the user
+num1=int(input("Enter first number :"))
+#-----------------------------------------------
+#input of second number from the user
+num2=int(input("Enter second number :"))
+#------------------------------------------------
+print("---------------------------------")
+#finding the greatest 
+if(num1>num2):
+    print(num1, "is greater than", num2)
+elif(num2>num1):
+    print(num2, "is greater than", num1)
+else:
+    print(num1, "and" ,num2, "are equal" )
+#------------------------------------------------       
+
+
+'''Output:
+Enter first number :67
+Enter second number :32
+---------------------------------
+67 is greater than 32  '''
+            
